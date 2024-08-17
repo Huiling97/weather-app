654056ae0f78c13a61709b86776a9ab2fc5e953a
April <huiling_loh_97@hotmail.com>
2024-08-17 12:12:18 +0800

    updated header

--- src/components/misc/heading.jsx
+++ src/components/misc/heading.jsx
@@ -16,6 +16,31 @@ const Heading = ({ data }) => {
     <div className='heading'>
       <p>Today's Weather</p>
       <img src={SunImage} alt='logo' className='heading__logo' />
+
+      <div className='allDetails'>
+        <div className='allTemp'>
+          <p className='heading__primary'>{data.temp}&deg;</p>
+          <div className='minMax-temp'>
+            <p>H: {data.maxTemp}&deg;</p>
+            <p>L: {data.minTemp}&deg;</p>
+          </div>
+          <div className='bold light'>
+            {data.city}, {data.country}
+          </div>
+        </div>
+        <div className='otherDetails light'>
+          <p>{formatTimestamp(data.time)}</p>
+          <p>Humidity: {data.humidity}%</p>
+          <p>{data.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className='heading'>
+      <p>Today's Weather</p>
+      <img src={SunImage} alt='logo' className='heading__logo' />
       <p className='heading__primary'>{data.temp}&deg;</p>
       <div className='heading__secondary'>
         <div className='heading__secondary--row main'>
