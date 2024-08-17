9ceea2be33ef78f5ec39e42fa9444a5639dda22d
April <huiling_loh_97@hotmail.com>
2024-08-17 11:41:45 +0800

    created table and cards

---
+++ src/components/misc/card.jsx
@@ -0,0 +1,40 @@
+import SearchIcon from '../../assets/images/Search.png';
+import DeleteIcon from '../../assets/images/Delete.png';
+import IconButton from './iconButton';
+import { formatTimestamp } from '../../utils/timestampHelper';
+import { useContext } from 'react';
+import { EntryContext } from '../../store/entryContext';
+
+const Card = ({ entry, handleClick }) => {
+  const { deleteEntry } = useContext(EntryContext);
+  const { id, city, country, time } = entry;
+
+  const handleDelete = () => {
+    deleteEntry(id);
+  };
+
+  return (
+    <div className='card'>
+      <div className='card__details'>
+        <p>
+          {city}, {country}
+        </p>
+        <p className='card__details--small'>{formatTimestamp(time)}</p>
+      </div>
+      <div className='card__icon-container'>
+        <IconButton
+          imgSrc={SearchIcon}
+          styleClass='card__icon--light'
+          handleClick={() => handleClick(city, country)}
+        />
+        <IconButton
+          imgSrc={DeleteIcon}
+          styleClass='card__icon--light'
+          handleClick={handleDelete}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/misc/table.jsx
@@ -0,0 +1,38 @@
+import isEmpty from 'lodash/isEmpty';
+import Card from './card';
+import { useContext } from 'react';
+import { EntryContext } from '../../store/entryContext';
+
+const Table = ({ entries, handleClick }) => {
+  // const {entries} = useContext(EntryContext);
+
+  const displayEntries = () => {
+    return entries.map((entry, index) => (
+      <Card
+        key={`${entry.id}-${entry.time}-${index}`}
+        entry={entry}
+        handleClick={handleClick}
+      />
+    ));
+  };
+
+  return (
+    <div className='table'>
+      <div className='table__data'>
+        {isEmpty(entries) ? (
+          <>
+            <p>No search history yet</p>
+            <p>Start searching for some data!</p>
+          </>
+        ) : (
+          <>
+            <p className='table__header'>Search history</p>
+            {displayEntries(entries)}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Table;
